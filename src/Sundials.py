7825908fc72f77201134665410a285b215b09f49
Christian Winther <3582459+chria@users.noreply.github.com>
2010-04-15 13:15:48 +0000

    Updated the documentation in the IDA code. Related to ticket:28 .

--- src/Sundials.py
+++ src/Sundials.py
@@ -42,7 +42,21 @@ class Sundials:
             self.Integrator = sundials_core.CVode_wrap(dim) #Creates a CVode integrator
     
     def _set_atol(self,atol):
-        """Sets the absolute tolerance(s)."""
+        """
+        Defines the absolute tolerance(s) that is to be used by the solver.
+        Can be set differently for each variable.
+        
+            Parameters:
+                atol    - Default '1.0e-6'.
+                
+                        - Should be a positive float or a numpy vector
+                          of floats.
+                        
+                            Example:
+                                atol = [1.0e-4, 1.0e-6]
+        
+        See SUNDIALS IDA documentation 4.5.2 for more details.
+        """
         if not isinstance(atol,float):
             if not isinstance(atol, list) and not isinstance(atol, N.ndarray):
                 raise Sundials_Exception('Absolute tolerance must be a float or a float vector.')
@@ -60,41 +74,109 @@ class Sundials:
                 raise Sundials_Exception('Absolute tolerance must be a positive (scalar) float or a positive (vector) float.')
             self.Integrator.abstol_ar=atol*N.ones(self.Integrator.dim)
             self.__atol=atol
+    
     def _get_atol(self):
-        """Returns the absolute tolerance(s)."""
+        """
+        Defines the absolute tolerance(s) that is to be used by the solver.
+        Can be set differently for each variable.
+        
+            Parameters:
+                atol    - Default '1.0e-6'.
+                
+                        - Should be a positive float or a numpy vector
+                          of floats.
+                        
+                            Example:
+                                atol = [1.0e-4, 1.0e-6]
+        
+        See SUNDIALS IDA documentation 4.5.2 for more details.
+        """
         return self.__atol
-    atol=property(_get_atol,_set_atol,doc='Absolute tolerance\n can be a float vector or a float scalar.')
+    
+    atol=property(_get_atol,_set_atol)
     
     def _set_rtol(self,rtol):
-        """Sets the relative tolerance."""
+        """
+        Defines the relative tolerance that is to be used by the solver.
+        
+            Parameters:
+                rtol    - Default '1.0e-6'.
+                
+                        - Should be a positive float.
+                        
+                            Example:
+                                rtol = 1.0e-4
+                                
+        """
         if not isinstance(rtol,float):
             raise Sundials_Exception('Relative tolerance must be a (scalar) float.')
         if rtol <= 0.0:
             raise Sundials_Exception('Relative tolerance must be a positive (scalar) float.')
         self.Integrator.reltol=rtol
         self.__rtol=rtol
+    
     def _get_rtol(self):
-        """Returns the relative tolerance."""
+        """
+        Defines the relative tolerance that is to be used by the solver.
+        
+            Parameters:
+                rtol    - Default '1.0e-6'.
+                
+                        - Should be a positive float.
+                        
+                            Example:
+                                rtol = 1.0e-4
+                                
+        """
         return self.__rtol
-    rtol=property(_get_rtol,_set_rtol,doc='Absolute tolerance\n can only be a float scalar.')
+        
+    rtol=property(_get_rtol,_set_rtol)
     
     def _set_max_h(self,max_h):
-        """Sets the maximal stepsize with the default value of infinity."""
+        """
+        Defines the maximal step-size that is to be used by the solver.
+        
+            Parameters:
+                maxh    - Default '0', which indicates that the maximal
+                          step-size is infinity.
+                          
+                        - Should be a float.
+                        
+                            Example:
+                                maxh = 0.01
+                                
+        """
         if not isinstance(max_h,float):
             raise Sundials_Exception('Maximal stepsize must be a (scalar) float.')
         if max_h < 0.0:
             raise Sundials_Exception('Maximal stepsize must be a positive (scalar) float.')
         self.Integrator.max_h=max_h
         self.__max_h=max_h
+    
     def _get_max_h(self):
-        """Returns the maximal stepsize."""
+        """
+        Defines the maximal step-size that is to be used by the solver.
+        
+            Parameters:
+                maxh    - Default '0', which indicates that the maximal
+                          step-size is infinity.
+                          
+                        - Should be a float.
+                        
+                            Example:
+                                maxh = 0.01
+                                
+        """
         return self.__max_h
-    maxh=property(_get_max_h,_set_max_h,doc='Maximal stepsize')    
+        
+    maxh=property(_get_max_h,_set_max_h)    
     
     def plot_stepsize_order(self):
         """
-        Plots the step-size used throughout the integration together with
-        the order used. Only able when using one-step mode.
+        Plots the step-size used through-out the integration together with
+        the order used. Only able when using one-step mode, 
+        
+        (number of communication points 'ncp' = 0).
         """
         if not self.Integrator.detailed_info:
             raise Sundials_Exception('There is no information. Either the problem' \
@@ -117,10 +199,14 @@ class Sundials:
     
     @property
     def stats(self):
-        """Attribute that returns the run-time statistics from the Integrator."""
+        """
+        Attribute that returns the run-time statistics from the Integrator.
+        """
         return self.Integrator.stats
     
     @property
     def disc_info(self):
-        """Attribute that returns information about an occured event."""
+        """
+        Attribute that returns information about an occured event.
+        """
         return [self.Integrator.event_time, self.Integrator.event_info]
