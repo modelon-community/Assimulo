c821f1b4823a774612ad748b9dfe365aff8b8acb
Christian Winther <3582459+chria@users.noreply.github.com>
2010-04-20 07:10:13 +0000

    Updated the setup files with information about the release number and homepage. Related to ticket:31 .

--- setup.py
+++ setup.py
@@ -10,11 +10,11 @@ else:
     lib_file = 'sundials_core.so'
 
 setup(name='Assimulo',
-      version='1.1',
+      version='b1.02',
       description='A package for solving ordinary differential equations',
-      author='Claus Fuhrer and Christian Andersson',
+      author='Claus Führer and Christian Andersson',
       author_email='claus@maths.lth.se chria@kth.se',
-      url='http://wwww',
+      url='http://wwww.jmodelica.org/assimulo',
       package_dir = {'Assimulo':'src'},
       packages=['Assimulo', 'Assimulo.lib'],
       package_data={'Assimulo.lib':[lib_file]}
