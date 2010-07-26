57ef3b2454c954aa505ea0d5fcb55ab3e7781efb
Christian Winther <3582459+chria@users.noreply.github.com>
2010-07-26 08:44:50 +0000

    Added the Radau5 solver for the ODE case. Missing tests for the solver. Related to ticket:59 .

---
+++ tests/test_Radau.py
@@ -0,0 +1,103 @@
+#!/usr/bin/env python 
+# -*- coding: utf-8 -*-
+
+# Copyright (C) 2010 Modelon AB
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, version 3 of the License.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program. If not, see <http://www.gnu.org/licenses/>.
+
+import nose
+import numpy as N
+
+class Test_Radau_Common:
+    """
+    Tests the common attributes of the Radau solvers.
+    """
+    
+    def test_fac1(self):
+        """
+        This tests the functionality of the property fac1.
+        """
+        pass
+    
+    
+    def test_fac2(self):
+        """
+        This tests the functionality of the property fac2.
+        """
+        pass
+    
+    def test_fnewt(self):
+        """
+        This tests the functionality of the property fnewt.
+        """
+        pass
+    
+    def test_h(self):
+        """
+        This tests the functionality of the property h.
+        """
+        pass
+    
+    def test_initial_step(self):
+        """
+        This tests the functionality of the property initial step.
+        """
+        pass
+    
+    def test_newt(self):
+        """
+        This tests the functionality of the property newt.
+        """
+        pass
+    
+    def test_quot1(self):
+        """
+        This tests the functionality of the property quot1.
+        """
+        pass
+        
+    def test_quot2(self):
+        """
+        This tests the functionality of the property quot2.
+        """
+        pass
+    
+    def test_safe(self):
+        """
+        This tests the functionality of the property safe.
+        """
+        pass
+    
+    def test_thet(self):
+        """
+        This tests the functionality of the property thet.
+        """
+        pass
+    
+    def test_usejac(self):
+        """
+        This tests the functionality of the property usejac.
+        """
+        pass
+        
+    def test_print_statistics(self):
+        """
+        This tests the functionality of the method print statistics.
+        """
+        pass
+    
+class Test_Explicit_Radau:
+    """
+    Tests the explicit Radau solver.
+    """
+    pass
