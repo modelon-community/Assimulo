cf421c98ed37960e05be5974b59343096a6cec67
chria <chria@df693ffa-1c7a-11df-a304-81b425dfc762>
2010-08-19 14:47:54 +0000

    Create a branch for the upcoming release.

---
+++ branches/Assimulo-1.1.x/doc/sphinx/source/download.rst
@@ -0,0 +1,26 @@
+
+
+=============
+Download
+=============
+
+Current version:
+
+    - :download:`Assimulo-1.0b2.zip <Assimulo-1.0b2.zip>`
+
+   
+See :doc:`installation` for instructions on setting up Assimulo.
+
+
+To retrieve the latest version using a subversion software use::
+
+    svn checkout https://svn.jmodelica.org/assimulo/tags/Assimulo-1.0b2
+
+To retrieve the latest (develop) version using a subversion software use::
+
+    svn checkout https://svn.jmodelica.org/assimulo/trunk
+
+
+.. warning::
+
+    The latest (develop) version may not function properly.
---
+++ branches/Assimulo-1.1.x/setup_source.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python 
+# -*- coding: utf-8 -*-
+from distutils.core import setup, Extension
+from Cython.Distutils import build_ext
+import numpy as N
+import sys as S
+import os as O
+
+incdirs = ''
+libdirs = ''
+
+if S.platform == 'win32':
+    incdirs = ''
+    libdirs = ''
+else:
+    incdirs = '/usr/local/include'
+    libdirs = '/usr/local/lib'
+
+copy_args=S.argv[1:]
+
+for x in S.argv[1:]:
+    if not x.find('--sundials-home'):
+        incdirs = O.path.join(x[16:],'include')
+        libdirs = O.path.join(x[16:],'lib')
+        copy_args.remove(x)
+    if not x.find('--prefix'):
+        copy_args[copy_args.index(x)] = x.replace('/',O.sep)
+
+if O.path.exists(O.path.join(O.path.join(incdirs,'cvode'), 'cvode.h')):
+    
+    cordir = O.path.join(O.path.join('src','lib'),'sundials_core.pyx')
+
+    setup(name='Assimulo_Source',
+      version='trunk',
+      description='A package for solving ordinary differential equations',
+      author='Claus Führer and Christian Andersson',
+      author_email='claus@maths.lth.se chria@kth.se',
+      url='http://wwww.jmodelica.org/assimulo',
+      package_dir = {'assimulo':'src'},
+      packages=['assimulo', 'assimulo.lib'],
+      cmdclass = {'build_ext': build_ext},
+      ext_package='assimulo',
+      ext_modules = [
+        Extension('lib.sundials_core',
+            [cordir],
+            include_dirs=[incdirs, N.get_include ()],
+            library_dirs=[libdirs],
+            libraries=['sundials_cvode','sundials_idas','sundials_nvecserial']),
+    ],
+    script_args=copy_args
+     )
+else:
+    raise Exception('Could not find Sundials. Recheck Sundials path.')
