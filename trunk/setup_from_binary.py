07505bf65ffe76541505df9d7b7f4f27e7abd41f
chria <chria@df693ffa-1c7a-11df-a304-81b425dfc762>
2010-09-23 15:08:50 +0000

    Updated the setup from binary file to also include the kinsol modules. Related to ticket:107

--- trunk/setup_from_binary.py
+++ trunk/setup_from_binary.py
@@ -3,13 +3,19 @@
 from distutils.core import setup, Extension
 import numpy as N
 import sys as S
+import os as O
 
 lib_file = ''
 
 if S.platform == 'win32':
     lib_file = 'sundials_core.pyd'
+    lib_file = 'sundials_kinsol_core.pyd'
+    suffix = '.pyd'
 else:
     lib_file = 'sundials_core.so'
+    lib_file = 'sundials_kinsol_core.so'
+    suffix = '.so'
+
 
 setup(name='Assimulo',
       version='trunk',
@@ -19,7 +25,7 @@ setup(name='Assimulo',
       url='http://wwww.jmodelica.org/assimulo',
       package_dir = {'assimulo':'src'},
       packages=['assimulo', 'assimulo.lib'],
-      package_data={'assimulo.lib':[lib_file]}
+      package_data={'assimulo':['lib'+O.sep+'*'+suffix]}
      )
 
 
