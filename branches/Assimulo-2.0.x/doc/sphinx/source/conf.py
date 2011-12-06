c29e4d89dcdb009098ce9d0ace4d5d54153d2937
chria <chria@df693ffa-1c7a-11df-a304-81b425dfc762>
2011-12-06 10:21:24 +0000

    Minor documentation update.

--- branches/Assimulo-2.0.x/doc/sphinx/source/conf.py
+++ branches/Assimulo-2.0.x/doc/sphinx/source/conf.py
@@ -146,6 +146,7 @@ html_static_path = ['_static']
 # If true, links to the reST sources are added to the pages.
 html_show_sourcelink = False
 html_copy_source = False
+html_show_sphinx = True
 
 # If true, an OpenSearch description file will be output, and all pages will
 # contain a <link> tag referring to it.  The value of this option must be the
