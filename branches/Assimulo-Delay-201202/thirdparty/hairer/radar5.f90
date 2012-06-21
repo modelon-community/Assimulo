63523c5cb88c87f5bdc415d3000398b02c1bae3b
tony <tony@df693ffa-1c7a-11df-a304-81b425dfc762>
2012-06-21 12:38:19 +0000

    Addition to last commit, missed a few writes.

--- branches/Assimulo-Delay-201202/thirdparty/hairer/radar5.f90
+++ branches/Assimulo-Delay-201202/thirdparty/hairer/radar5.f90
@@ -2151,7 +2151,7 @@ C ---     BP IS WRONG! REPEAT
         ELSE
 C ---     LOOK FOR A BP        
           HP=H*0.99D0
-          WRITE(6,*) 'Calling BPDTCT from row 2154.'
+C          WRITE(6,*) 'Calling BPDTCT from row 2154.'
           CALL BPDTCT(N,X,HP,Y,ARGLAG,RPAR,IPAR,UCONT,GRID,NLAGS, 
      &                FIRST,LAST,XEND,IGRID,BPV,IBP,ILBP,BPP,BPD,
      &                KMAX,PHI,PAST,IPAST,NRDS) 
@@ -2394,8 +2394,8 @@ C
 C ----------------------------- 
       
 C ------ FINAL POINT                               
-         WRITE(6, *) 'X:', X
-         WRITE(6, *) 'IGRID:', IGRID
+C         WRITE(6, *) 'X:', X
+C         WRITE(6, *) 'IGRID:', IGRID
          IF (LAST) THEN 
  45         CONTINUE
             IF (IGRID.EQ.NGRID) THEN 
@@ -2423,8 +2423,8 @@ C              LEFT=.FALSE.
                HNEW=MIN(HNEW,H)
             END IF 
          END IF 
-         WRITE(6,*) 'Before updating hnew and hopt'
-         WRITE(6, *) 'IGRID:', IGRID
+C         WRITE(6,*) 'Before updating hnew and hopt'
+C         WRITE(6, *) 'IGRID:', IGRID
          HNEW=MIN(HNEW,HMAXN) 
          HOPT=MIN(H,HNEW) 
          IF (REJECT) HNEW=MIN(HNEW,H)  
@@ -2964,7 +2964,7 @@ C        WRITE(6,*) 'In BPDTCT, ILBP = ', ILBP
         EPSILON=1.D-10
         ALLOCATE(YADV(N)) 
         COMPAR=UROUND*MAX(ABS(X),ABS(X+H)) 
-        WRITE(6,*) 'COMPAR:', COMPAR, X, X+H, UROUND
+C        WRITE(6,*) 'COMPAR:', COMPAR, X, X+H, UROUND
         
       XLAST=UCONT(LRC+1) 
         HLAST=UCONT(LRC+2) 
