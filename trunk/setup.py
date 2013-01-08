78fa00d63ffef238737ae09a2cb235b78c23fa1d
chria <chria@df693ffa-1c7a-11df-a304-81b425dfc762>
2013-01-08 10:23:08 +0000

    Updated changelog and updated license to LGPL. Related to ticket:262 and ticket:261

--- trunk/setup.py
+++ trunk/setup.py
@@ -1,18 +1,18 @@
 #!/usr/bin/env python 
 # -*- coding: utf-8 -*-
 
-# Copyright (C) 2011 Modelon AB
+# Copyright (C) 2010 Modelon AB
 #
 # This program is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
+# it under the terms of the GNU Lesser General Public License as published by
 # the Free Software Foundation, version 3 of the License.
 #
 # This program is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-# GNU General Public License for more details.
+# GNU Lesser General Public License for more details.
 #
-# You should have received a copy of the GNU General Public License
+# You should have received a copy of the GNU Lesser General Public License
 # along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 #from distutils.core import setup, Extension
@@ -466,7 +466,7 @@ NAME = "Assimulo"
 AUTHOR = "C. Andersson, C. Führer, J. Åkesson, M. Gäfvert"
 AUTHOR_EMAIL = "chria@maths.lth.se"
 VERSION = "trunk"
-LICENSE = "GPL"
+LICENSE = "LGPL"
 URL = "http://www.jmodelica.org/assimulo"
 DOWNLOAD_URL = "http://www.jmodelica.org/assimulo"
 DESCRIPTION = "A package for solving ordinary differential equations and differential algebraic equations."
