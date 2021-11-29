2251211633fdc00a97d284663b36fa0009607d8b
PeterMeisrimelModelon <92585725+PeterMeisrimelModelon@users.noreply.github.com>
2021-11-29 15:45:08 +0100

    fixing string based properties

--- src/lib/radau_core.py
+++ src/lib/radau_core.py
@@ -487,6 +487,10 @@ class Radau_Common(object):
         return self.options["linear_solver"]
 
     def _set_linear_solver(self, linear_solver):
+        try:
+            linear_solver.lower()
+        except:
+            raise Radau_Exception("linear_solver parameters needs to be the STRING 'dense' or 'sparse'. Set value: {}, type: {}".format(linear_solver, type(linear_solver)))
         if linear_solver.lower() == "dense":
             pass
         elif linear_solver.lower() == "sparse":
