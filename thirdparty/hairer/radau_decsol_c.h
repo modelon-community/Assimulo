8de34b097f86122fed6fda6ffb64dd47280fbb62
PeterMeisrimelModelon <92585725+PeterMeisrimelModelon@users.noreply.github.com>
2021-12-15 12:00:12 +0100

    fixed setup.py for superLU inclusion

--- thirdparty/hairer/radau_decsol_c.h
+++ thirdparty/hairer/radau_decsol_c.h
@@ -2,6 +2,8 @@
 #define RADAU_DECSOL_C_H
 
 #include <stdint.h>
+#include "radau5_superlu_double.h"
+#include "radau5_superlu_complex.h"
 
 #define TRUE_ (1)
 #define FALSE_ (0)
@@ -24,6 +26,14 @@ typedef int (*FP_CB_mas)(integer, doublereal*, integer*, doublereal*,
 typedef int (*FP_CB_solout)(integer*, doublereal*, doublereal*, doublereal*,
                             doublereal*, doublereal*, integer*, integer*,
                             doublereal*, integer*, integer*, void*);
+
+typedef struct {double r, i;} doublecomplex;
+
+typedef int (*FP_CB_jac_sparse)(double, int, int*, double*, int*, int*, void*);
+typedef int (*FP_CB_assemble_sys_d)(int, double, int *, double *, int *, int *, double *, int *, int *, int, double*);
+typedef int (*FP_CB_assemble_sys_z)(int, double, double, int *, double *, int *, int *, doublecomplex *, int *, int *, int, double*);
+
+int superlu_setup_z(SuperLU_aux_z *, double, double, double *, int *, int *, int, double*, FP_CB_assemble_sys_z);
 
 int radau5_c(integer n, FP_CB_f fcn, void* fcn_PY, doublereal *x, doublereal *
             y, doublereal *xend, doublereal *h__, doublereal *rtol, doublereal *
