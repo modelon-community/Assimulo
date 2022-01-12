a39b8e02f3e0e71652ac02c4d70e70737c6aafef
PeterMeisrimelModelon <92585725+PeterMeisrimelModelon@users.noreply.github.com>
2022-01-12 14:44:34 +0100

    testing

--- setup.py
+++ setup.py
@@ -520,10 +520,6 @@ class Assimulo_prepare(object):
         ext_list[-1].name = "assimulo.lib.radau5_c_py"
         if 'win' in self.platform:
             ext_list[-1].library_dirs = [os.path.join(self.SLUincdir, "..", "lib"), self.libdirs]
-            # ext_list[-1].library_dirs = [os.path.join(self.SLUincdir, "..", "lib"), self.BLASdir, self.libdirs]
-            print("LIBDIRS EXT: ", ext_list[-1].library_dirs)
-            print("LIBDIRS: ", self.libdirs)
-            # ext_list[-1].libraries = ['superlu_mt_OPENMP', 'blas_OPENMP', 'blas', 'm', 'gomp']
             ext_list[-1].libraries = ['superlu_mt_OPENMP', 'blas_OPENMP']
             ext_list[-1].extra_compile_args += ["-D__OPENMP"]
         else:
