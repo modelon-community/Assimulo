45916bd9aa8fe4c2359ba22952361eb1d3d18e4e
PeterMeisrimelModelon <92585725+PeterMeisrimelModelon@users.noreply.github.com>
2022-01-19 10:03:45 +0100

    testing, disabling a test

--- src/solvers/radau5.py
+++ src/solvers/radau5.py
@@ -120,7 +120,7 @@ class Radau5ODE(Radau_Common,Explicit_ODE):
                 raise Radau_Exception("Sparse Linear solver not supported for Fotran based solver, try setting 'solver' = 'c' instead.")
             if not self.usejac:
                 # raise Radau_Exception("Sparse Linear solver not compatible with numerically computed Jacobians. Provide a sparse Jacobian or instead use 'linear_solver' = 'dense'.")
-                self.options["linear_solver"] == "dense"
+                self.linear_solver = "dense"
         if not self.solver_module_imported:
             self.solver = self.options["solver"] 
             
